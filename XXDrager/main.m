ee7b614c1e85023bf6a546add3c3ac34c74d0150
shine <shine@shinedeMacBook-Pro.local>
2017-03-06 15:40:23 +0800

    Initial Commit

---
+++ XXDrager/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  XXDrager
+//
+//  Created by shine on 2017/3/6.
+//  Copyright © 2017年 shine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
